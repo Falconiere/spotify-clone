598387db3d468f804f37f47ce76f29e03c2c5635
Falconiere R. Barbosa <falconieer@gmail.com>
2022-03-05 04:48:03 -0300

    Removing package

--- src/providers/Api/index.tsx
+++ src/providers/Api/index.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { QueryClient, QueryClientProvider } from "react-query";
-const queryClient = new QueryClient();
 export function ApiProvider({ children }: { children: React.ReactNode }) {
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  return <>{children}</>;
 }
